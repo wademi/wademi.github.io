b5843e0989e56846c32f9799bd7d474b79903c5e
wademi <31478676+wademi@users.noreply.github.com>
2018-03-09 11:11:18 -0500

    Update include.js

--- include.js
+++ include.js
@@ -2,7 +2,7 @@ function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
+  for (i = 0; i < z.length; i++) {  
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
